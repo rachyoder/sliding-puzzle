98a1a0fcb43ba3b65084e1ed8474d964b72ca7e5
Rachael Yoder <rachaelyoder@c1092933.localdomain>
2019-10-02 10:30:19 -0400

    set up basic constructors

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,19 +1,37 @@
-function Tile(x,y,isFilled) {
-    this.x = x;
-    this.y = y;
+/* Global Variables */
+// let gridPos = [1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1];
+
+function Tile(heightSize, widthSize, position, isFilled) {
+    this.heightSize = heightSize;
+    this.widthSize = widthSize;
+    this.position = position;
     this.isFilled = isFilled;
 }
 
 
 /* Sliding Tiles Constructor */
-function SlidingTile(x,y,isFilled) {
-    Tile.call(this,x,y)
+function SlidingTile(heightSize, widthSize, position, isFilled, color) {
+    Tile.call(this, heightSize, widthSize, position);
     this.isFilled = true;
-
+    this.color = "rgb(102,102,153)";
 }
-SlidingTile.prototype = object.create(Tile.prototype);
+SlidingTile.prototype = Object.create(Tile.prototype);
 Object.defineProperty(SlidingTile.prototype, 'constructor', {
     Value: SlidingTile,
+    enumerable: false,
+    writeable: true
+});
+
+
+/* Empty Tile Constructor */
+function EmptyTile(heightSize, widthSize, position, isFilled) {
+    Tile.call(this, heightSize, widthSize, position);
+    this.isFilled = false;
+
+}
+EmptyTile.prototype = Object.create(Tile.prototype);
+Object.defineProperty(EmptyTile.prototype, 'constructor', {
+    Value: EmptyTile,
     enumerable: false,
     writeable: true
 });
