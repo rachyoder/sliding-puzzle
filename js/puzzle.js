b52cc7370dc2baa25c87089569acb3b4a30e538b
Rachael Yoder <wayoder4@gmail.com>
2019-10-07 10:05:45 -0400

    Restricted movement to rows and columns on grid

--- js/puzzle.js
+++ js/puzzle.js
@@ -43,12 +43,7 @@ for (i = 0; tiles.length < 16; i++) {
     tiles.push(tile);
 }
 
-/* Renders Board for movement */
-function renderBoard(gridId, blankPos) {
-    console.log({ gridId });
-    console.log({ blankPos });
 
-}
 
 /* Changes x and y position in Object */
 function movement(e) {
@@ -59,38 +54,53 @@ function movement(e) {
     let clickedId = this.id;
     clickedId = Number(clickedId);
     //console.log({e});
-    console.log( clickedId-4 );
-    console.log();
-    if (
-        tiles[clickedId - 1].isBlank == 1 ||
-        tiles[clickedId + 1].isBlank == 1 ||
-        tiles[clickedId - 4].isBlank == 1 ||
-        tiles[clickedId + 4].isBlank == 1
-    ) {
-        xTemp = tiles[clickedId].x;
-        yTemp = tiles[clickedId].y;
-        console.log({ xTemp, yTemp });
-        for (let j = 0; j < tiles.length; j++) {
-            if (tiles[j].isBlank == 1) {
-                blankCurrentPos = tiles[j].tileId;
-                blankVerify = tiles[j].isBlank;
-
-            }
+    //console.log({clickedId});
+    
+    if (tiles[clickedId - 1] != undefined && clickedId % 4 != 0) {
+        if (tiles[clickedId - 1].isBlank == 1) {
+            checkTileMovement(clickedId);
         }
-        // Swapping tile postiion between clicked and blank
-        tiles[clickedId].x = tiles[blankCurrentPos].x;
-        tiles[clickedId].y = tiles[blankCurrentPos].y;
-        tiles[clickedId].isBlank = 1;
-        tiles[blankCurrentPos].x = xTemp;
-        tiles[blankCurrentPos].y = yTemp;
-        tiles[blankCurrentPos].isBlank = 0;
-
-
-        // y * 4 + x
-        let tileOne = document.getElementById(clickedId);
-        let tileTwo = document.getElementById(blankCurrentPos);
-        tileOne.innerHTML = tiles[tileOne.id].y * 4 + tiles[tileOne.id].x;
-        tileTwo.innerHTML = tiles[tileTwo.id].y * 4 + tiles[tileTwo.id].x;
-
     }
+    if (tiles[clickedId + 1] != undefined && (clickedId + 1) % 4 != 0) {
+        if (tiles[clickedId + 1].isBlank == 1) {
+            checkTileMovement(clickedId);
+        }
+    }
+    if (tiles[clickedId - 4] != undefined) {
+        if (tiles[clickedId - 4].isBlank == 1) {
+            checkTileMovement(clickedId);
+        }
+    }
+    if (tiles[clickedId + 4] != undefined) {
+        if (tiles[clickedId + 4].isBlank == 1) {
+            checkTileMovement(clickedId);
+        }
+    }
+}
+
+function checkTileMovement(id) {
+    xTemp = tiles[id].x;
+    yTemp = tiles[id].y;
+    //console.log({ xTemp, yTemp });
+    for (let j = 0; j < tiles.length; j++) {
+        if (tiles[j].isBlank == 1) {
+            blankCurrentPos = tiles[j].tileId;
+            blankVerify = tiles[j].isBlank;
+
+        }
+    }
+    // Swapping tile postiion between clicked and blank
+    tiles[id].x = tiles[blankCurrentPos].x;
+    tiles[id].y = tiles[blankCurrentPos].y;
+    tiles[id].isBlank = 1;
+    tiles[blankCurrentPos].x = xTemp;
+    tiles[blankCurrentPos].y = yTemp;
+    tiles[blankCurrentPos].isBlank = 0;
+
+
+    // y * 4 + x
+    let tileOne = document.getElementById(id);
+    let tileTwo = document.getElementById(blankCurrentPos);
+    tileOne.innerHTML = tiles[tileOne.id].y * 4 + tiles[tileOne.id].x;
+    tileTwo.innerHTML = tiles[tileTwo.id].y * 4 + tiles[tileTwo.id].x;
 }
