b2cce16edcadeb5ecbcfbffe821aacb65bb58158
Rachael Yoder <wayoder4@gmail.com>
2019-10-07 14:32:52 -0400

    win conditions and randomize board

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
-var app = document.getElementById("app");
-
+let app = document.getElementById("app");
 
 // Board Setup
 function init() {
     app.setAttribute("class", "container");
-    var row = document.createElement("div");
-    var col = document.createElement("col");
-    row.setAttribute("class", "row");
     for (i = 0; i < 3; i++) {
+        let row = document.createElement("div");
+        let col = document.createElement("col");
+        row.setAttribute("class", "row m-3");
         if (i === 0) {
             col.setAttribute("class", "col-12");
-            var head = document.createElement("h1");
-            head.setAttribute("class", "display-5");
+            let head = document.createElement("h1");
+            head.setAttribute("class", "display-5 text-center");
             head.innerHTML = "Sliding Puzzle";
+            let sub = document.createElement("h6");
+            sub.setAttribute("class","text-center text-muted");
+            sub.setAttribute("id", "win");
+            sub.innerHTML = "Click the tiles to solve the puzzle";
             col.appendChild(head);
+            col.appendChild(sub);
             row.appendChild(col);
         } else if (i === 1) {
             // Setup all Boxes
-            for (var j = 0; j < 16; j++) {
-                var tileSpace = document.createElement("div");
+            for (let j = 0; j < 16; j++) {
+                let tileSpace = document.createElement("div");
                 tileSpace.setAttribute("class","col-3 p-0 border text-center");
                 tileSpace.setAttribute("id", j);
                 tiles[j].populate(tileSpace);
                 tileSpace.addEventListener("click", movement);
+                tiles[j].fill(tileSpace);
                 row.appendChild(tileSpace);
             }
+        } else if ( i ===  2) {
+            let randomBtn = document.createElement("button");
+            let uploadBtn = document.createElement("button");
+            let uploadIco = document.createElement("i");
+
+            randomBtn.setAttribute("class", "btn btn-primary float-right");
+            randomBtn.setAttribute("id", "random");
+            randomBtn.innerHTML = "RANDOMIZE";
+            randomBtn.addEventListener("click", randomizeBoard);
+
+            uploadBtn.setAttribute("class", "btn btn-primary float-left");
+            uploadBtn.setAttribute("id", "upload");
+            uploadIco.setAttribute("class", "fas fa-upload");
+            // uploadBtn.innerHTML = '<i class="fas fa-upload"></i>';
+        
+            col.setAttribute("class", "col-12");
+            col.appendChild(randomBtn);
+            uploadBtn.appendChild(uploadIco);
+            col.appendChild(uploadBtn);
+            row.appendChild(col);
         }
         app.appendChild(row);
     }
--- js/puzzle.js
+++ js/puzzle.js
@@ -1,36 +1,43 @@
-/* Global Variables */
-let xPos = 0;
-let yPos = 0;
-let blank = 1;
-
-
 /* Tile Constructor */
-function Tile(idx, tileId, x, y, isBlank, currentPos) {
+function Tile(idx, tileId, x, y, isBlank, color) {
     this.idx = idx;
     this.tileId = tileId;
     this.x = x;
     this.y = y;
     this.isBlank = isBlank;
-    this.currentPos = y * 4 + x;
     let text = document.createElement("div");
     text.innerHTML = idx;
     this.content = text;
+    //this.color = color;
 }
 
+/* Fill columns with Tiles */
 Tile.prototype.populate = function (col) {
     col.appendChild(this.content);
 }
 
+/* Add color to each Tile */
+Tile.prototype.fill = function (col) {
+    col.setAttribute("style", "background-color: " + this.color);
+}
+
 /* Create Tiles Array */
 let tiles = [];
+let xPos = 0;
+let yPos = 0;
+let blank = 1;
+// let color = "rgb(0,0,0)"
+
 for (i = 0; tiles.length < 16; i++) {
     let tile = new Tile(
         i,
         i,
         xPos,
         yPos,
-        blank
+        blank,
+        //color
     );
+
     if (xPos < 3) {
         xPos++;
     } else if (xPos == 3) {
@@ -39,68 +46,93 @@ for (i = 0; tiles.length < 16; i++) {
     }
     if (blank == 1) {
         blank--;
+        // color = "rgb(255,255,255)";
     }
     tiles.push(tile);
 }
 
-
-
-/* Changes x and y position in Object */
-function movement(e) {
+/* Checks for Valid Tile Change */
+function movement() {
     let xTemp = null;
     let yTemp = null;
-    let blankCurrentPos = null;
+    let blankId = null;
     let blankVerify = null;
     let clickedId = this.id;
     clickedId = Number(clickedId);
-    //console.log({e});
-    //console.log({clickedId});
-    
+
     if (tiles[clickedId - 1] != undefined && clickedId % 4 != 0) {
         if (tiles[clickedId - 1].isBlank == 1) {
-            checkTileMovement(clickedId);
+            changeTileMovement(clickedId);
         }
     }
     if (tiles[clickedId + 1] != undefined && (clickedId + 1) % 4 != 0) {
         if (tiles[clickedId + 1].isBlank == 1) {
-            checkTileMovement(clickedId);
+            changeTileMovement(clickedId);
         }
     }
     if (tiles[clickedId - 4] != undefined) {
         if (tiles[clickedId - 4].isBlank == 1) {
-            checkTileMovement(clickedId);
+            changeTileMovement(clickedId);
         }
     }
     if (tiles[clickedId + 4] != undefined) {
         if (tiles[clickedId + 4].isBlank == 1) {
-            checkTileMovement(clickedId);
+            changeTileMovement(clickedId);
         }
     }
+    checkWin();
 }
 
-function checkTileMovement(id) {
+/* Changes Tile Position */
+function changeTileMovement(id) {
     xTemp = tiles[id].x;
     yTemp = tiles[id].y;
-    //console.log({ xTemp, yTemp });
     for (let j = 0; j < tiles.length; j++) {
         if (tiles[j].isBlank == 1) {
-            blankCurrentPos = tiles[j].tileId;
+            blankId = tiles[j].tileId;
             blankVerify = tiles[j].isBlank;
 
         }
     }
-    // Swapping tile postiion between clicked and blank
-    tiles[id].x = tiles[blankCurrentPos].x;
-    tiles[id].y = tiles[blankCurrentPos].y;
+    tiles[id].x = tiles[blankId].x;
+    tiles[id].y = tiles[blankId].y;
     tiles[id].isBlank = 1;
-    tiles[blankCurrentPos].x = xTemp;
-    tiles[blankCurrentPos].y = yTemp;
-    tiles[blankCurrentPos].isBlank = 0;
+    tiles[blankId].x = xTemp;
+    tiles[blankId].y = yTemp;
+    tiles[blankId].isBlank = 0;
 
-
-    // y * 4 + x
     let tileOne = document.getElementById(id);
-    let tileTwo = document.getElementById(blankCurrentPos);
+    let tileTwo = document.getElementById(blankId);
     tileOne.innerHTML = tiles[tileOne.id].y * 4 + tiles[tileOne.id].x;
     tileTwo.innerHTML = tiles[tileTwo.id].y * 4 + tiles[tileTwo.id].x;
+}
+
+function checkWin() {
+    let winCount = 0;
+    let winDisplay = document.getElementById("win");
+    for (let k = 0; k < tiles.length; k++) {
+        if(tiles[k].idx == tiles[k].y * 4 + tiles[k].x) {
+            winCount ++;
+        }
+    }
+    if (winCount == 16){
+        winDisplay.innerHTML = "You Win!";
+    } else {
+        winDisplay.innerHTML = "Click the tiles to solve the puzzle";
+    }
+}
+
+/* Random Number 0-15 */
+function randomInt() {
+    let int = Math.floor(Math.random() * Math.floor(16));
+    return int.toString();
+}
+
+/* Randomize Board Placement */
+function randomizeBoard() {
+    let r = 0;
+    do {
+        document.getElementById(randomInt()).click();
+        r++;
+    } while(r < 1000);
 }
